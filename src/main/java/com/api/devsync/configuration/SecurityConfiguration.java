311383163c8926d38e642daedc21b468a936e09a
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2025-08-02 19:14:51 +0300

    bug-fix cors error

--- src/main/java/com/api/devsync/configuration/SecurityConfiguration.java
+++ src/main/java/com/api/devsync/configuration/SecurityConfiguration.java
@@ -39,10 +39,10 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // React dev server
+        configuration.setAllowedOrigins(List.of("https://devsyncweb.site", "http://localhost:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true); // Cookie veya Authorization header gönderiyorsan gerekli
+        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);

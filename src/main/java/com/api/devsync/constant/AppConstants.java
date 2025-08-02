8257ed4397baabe0a6399a372edc6cdbaca0f0d7
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2025-08-03 00:39:26 +0300

    webhook url changed http to https

--- src/main/java/com/api/devsync/constant/AppConstants.java
+++ src/main/java/com/api/devsync/constant/AppConstants.java
@@ -1,5 +1,5 @@
 package com.api.devsync.constant;
 
 public class AppConstants {
-    public static final String webhookUrl = "http://devsyncweb.site/api/git-webhook/webhook/pull-request";
+    public static final String webhookUrl = "https://devsyncweb.site/api/git-webhook/webhook/pull-request";
 }

5fb918ae867010b0d9f47089c9cee0323bb53310
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2025-08-03 19:05:07 +0300

    fix

--- src/main/java/com/api/devsync/model/dto/CommitAnalysisDto.java
+++ src/main/java/com/api/devsync/model/dto/CommitAnalysisDto.java
@@ -3,12 +3,9 @@ package com.api.devsync.model.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.UUID;
-
 @Getter @Setter
 public class CommitAnalysisDto extends AnalyzeDto {
-    private UUID id;
-    private String hash;
+    private String id;
     private String message;
     private PullRequestAnalysisDto pullRequestAnalyze;
 }

4c151d238b91c3e564605e404fde396829e07198
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2025-08-03 17:51:16 +0300

    uuid bug fix

--- src/main/java/com/api/devsync/entity/Commit.java
+++ src/main/java/com/api/devsync/entity/Commit.java
@@ -21,7 +21,7 @@ public class Commit {
     private PullRequest pullRequest;
 
     @OneToOne
-    @JoinColumn(name = "hash", referencedColumnName = "hash")
+    @JoinColumn(name = "hash", referencedColumnName = "hash", insertable = false, updatable = false)
     private CommitAnalysis analysis;
 
     public Commit(String hash, String message) {
